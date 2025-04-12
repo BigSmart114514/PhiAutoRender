975bec726a5a4e26207703ef763e145339e8a7de
Dehou23333 <104069850+Dehou23333-awa@users.noreply.github.com>
2025-04-12 20:40:53 +0800

    Update download.py

--- download.py
+++ download.py
@@ -4,5 +4,4 @@ import os
 if __name__ == "__main__":
     url = taptap.taptap(165287)
     print(url)
-    os.system("wget %s" % url)
-    os.system("mv *.apk phigros.apk")
+    os.system(f"wget -O phigros.apk {url}")
